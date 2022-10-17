4814b4993946435f07a1f2b4b9f8e485cad480ce
Hridoy <hriodykumarshil12@gmail.com>
2022-10-17 20:40:39 +0600

    Accessing Redux Store from Components (Applying Redux)

--- src/components/body/Home.js
+++ src/components/body/Home.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+const mapStateToProps = state => {
+    console.log("mapStateToProps: ", state);
+    return {
+        dishes: state.dishes,
+        sample: state.sample
+    }
+}
 
 class Home extends Component {
+    componentDidMount() {
+        console.log("Home State: ", this.state);
+        console.log("Home Props: ", this.props);
+    }
     render() {
         document.title = "Bohubrihi Restaurant";
         return (
@@ -11,4 +24,4 @@ class Home extends Component {
     }
 }
 
-export default Home;
+export default connect(mapStateToProps)(Home);
