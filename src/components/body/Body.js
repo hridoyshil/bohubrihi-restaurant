4b2f5edb001d5a03024150a42d25c3860a94bab6
Hridoy <hriodykumarshil12@gmail.com>
2022-10-17 13:11:07 +0600

    Adding Routes
    Routes  not works

--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -3,11 +3,21 @@ import Menu from "./Menu";
 import Home from "./Home";
 import Contact from "./Contact";
 import About from "./About";
+import Header from "../header/Header";
+import { Route, Redirect } from 'react-router-dom';
+
 
 const Body = () => {
     return (
-        <div>
 
+        <div>
+            <Header />
+            {/* <Route path="/home" exact component={Home} />
+            <Route path="/menu" exact component={Menu} />
+            <Route path="/contact" exact component={Contact} />
+            <Route path="/about" exact component={About} />
+            <Redirect from="/" to="/home" /> */}
+            <Menu />
         </div>
     );
 }
